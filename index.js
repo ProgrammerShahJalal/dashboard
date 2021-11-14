8655a08aab79720403675001bebd4127e6bbfddd
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-11-14 17:17:38 +0600

    card add

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 /* menu toggle */
 let toggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
 
+toggle.onclick = function () {
+    navigation.classList.toggle('active')
+    main.classList.toggle('active')
+}
 
-/* navigation list */
+/* add hover class in selected list item*/
 let list = document.querySelectorAll('.navigation li');
 function activeLink() {
     list.forEach((item) =>
